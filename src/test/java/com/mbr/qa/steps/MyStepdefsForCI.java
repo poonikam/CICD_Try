eb0603efade1e870c14cdbf8b2feb08ac286744c
poonikam <pooja-ramesh.nikam@capgemini.com>
2022-01-21 19:17:34 +0530

    abc

---
+++ src/test/java/com/mbr/qa/steps/MyStepdefsForCI.java
@@ -0,0 +1,12 @@
+package com.mbr.qa.steps;
+
+import io.cucumber.java.en.Given;
+
+
+public class MyStepdefsForCI {
+    @Given("I launch the Calender new CI")
+    public void userInvokesTheBrowser() {
+        System.out.println("abc123");
+
+    }
+}
